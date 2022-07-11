190364ad9eb78e35a3d1fce13ec6cdda81008f64
Paul Grimal <grimal-paul@outlook.fr>
2022-07-11 13:59:55 +0200

    preparation embedding

--- processing/embedding_image.py
+++ processing/embedding_image.py
@@ -17,8 +17,11 @@ import torch
 from cvlep.CLIPT5.vis_encoder import get_vis_encoder, _transform
 from cairosvg import svg2png
 from io import BytesIO
+import json
 
 disable_caching()
+
+log_file = []
 
 # load Fastercnn
 def load_frcnn(config_path, model_path):
@@ -39,6 +42,7 @@ def open_image(path):
         return Image.open(BytesIO(png))
     else:
         return Image.open(path)
+
 def item_CLIP_embedding(item, key_image, key_image_embedding, image_path, transform, vis_encoder, batch_size):
     # mapping function to embed images with clip
     # can embed single image or list of image
@@ -70,63 +74,59 @@ def embed_with_CLIP(dataset_path, backbone, **kwargs):
 
 def item_fasterRCNN_embedding(item, key_image, key_image_embedding, image_process, image_path, frcnn, frcnn_cfg, batch_size):
     list_images = item[key_image]
+    global log_file
     #TODO: batch max number of image and concatenate results !
-    if isinstance(list_images,list):
-        images = list()
-        for image_name in list_images:
-            images.append(str(Path(image_path) / image_name))
-        # if len(images) > batch_size:
-        #     nb_batch = images.shape[0] // batch_size
-        #     remainder= images.shape[0] % batch_size
-        #     boxes = []
-        #     features = []
-        #     for i in range(0, nb_batch*batch_size, batch_size):
-        #         im = images[:batch_size]
-        #         images = images[:batch_size]
-        #         images, sizes, scales_yx = image_process(im)
-            
-        images, sizes, scales_yx = image_process(images)
-        if images.shape[0] > batch_size:
-            nb_batch = images.shape[0] // batch_size
-            remainder= images.shape[0] % batch_size
-            boxes = torch.empty((images.shape[0],frcnn_cfg.max_detections,4))
-            features = torch.empty((images.shape[0],frcnn_cfg.max_detections, 2048))
-            for i in range(0, nb_batch*batch_size, batch_size):
-                output_dict = frcnn(
-                    images[i:batch_size+i,:,:,:],
-                    sizes[i:batch_size+i,:],
-                    scales_yx=scales_yx[i:batch_size+i,:],
-                    padding='max_detections',
-                    max_detections=frcnn_cfg.max_detections,
-                    return_tensors='pt'
-                )
-                boxes[i:batch_size+i,:,:]=output_dict.get("normalized_boxes")
-                features[i:batch_size+i,:,:]=output_dict.get("roi_features")
-            if remainder > 0:
-                output_dict = frcnn(
-                    images[-remainder:,:,:,:],
-                    sizes[-remainder:,:],
-                    scales_yx=scales_yx[-remainder:,:],
-                    padding='max_detections',
-                    max_detections=frcnn_cfg.max_detections,
-                    return_tensors='pt'
-                )
-                boxes[-remainder:,:,:]=output_dict.get("normalized_boxes")
-                features[-remainder:,:,:]=output_dict.get("roi_features")
-            item[f'{key_image_embedding}_boxes'] = boxes
-            item[f'{key_image_embedding}_features'] = features
-    else:
-        images, sizes, scales_yx = image_process(str(Path(image_path)/list_images))
-        output_dict = frcnn(
-            images,
-            sizes,
-            scales_yx=scales_yx,
-            padding='max_detections',
-            max_detections=frcnn_cfg.max_detections,
-            return_tensors='pt'
-        )
-        item[f'{key_image_embedding}_boxes'] = output_dict.get("normalized_boxes")
-        item[f'{key_image_embedding}_features'] = output_dict.get("roi_features")
+    try:
+        if isinstance(list_images,list):
+            images = list()
+            for image_name in list_images:
+                images.append(str(Path(image_path) / image_name))
+            images, sizes, scales_yx = image_process(images)
+            if images.shape[0] > batch_size:
+                nb_batch = images.shape[0] // batch_size
+                remainder= images.shape[0] % batch_size
+                boxes = torch.empty((images.shape[0],frcnn_cfg.max_detections,4))
+                features = torch.empty((images.shape[0],frcnn_cfg.max_detections, 2048))
+                for i in range(0, nb_batch*batch_size, batch_size):
+                    output_dict = frcnn(
+                        images[i:batch_size+i,:,:,:],
+                        sizes[i:batch_size+i,:],
+                        scales_yx=scales_yx[i:batch_size+i,:],
+                        padding='max_detections',
+                        max_detections=frcnn_cfg.max_detections,
+                        return_tensors='pt'
+                    )
+                    boxes[i:batch_size+i,:,:]=output_dict.get("normalized_boxes")
+                    features[i:batch_size+i,:,:]=output_dict.get("roi_features")
+                if remainder > 0:
+                    output_dict = frcnn(
+                        images[-remainder:,:,:,:],
+                        sizes[-remainder:,:],
+                        scales_yx=scales_yx[-remainder:,:],
+                        padding='max_detections',
+                        max_detections=frcnn_cfg.max_detections,
+                        return_tensors='pt'
+                    )
+                    boxes[-remainder:,:,:]=output_dict.get("normalized_boxes")
+                    features[-remainder:,:,:]=output_dict.get("roi_features")
+                item[f'{key_image_embedding}_boxes'] = boxes
+                item[f'{key_image_embedding}_features'] = features
+        else:
+            images, sizes, scales_yx = image_process(str(Path(image_path)/list_images))
+            output_dict = frcnn(
+                images,
+                sizes,
+                scales_yx=scales_yx,
+                padding='max_detections',
+                max_detections=frcnn_cfg.max_detections,
+                return_tensors='pt'
+            )
+            item[f'{key_image_embedding}_boxes'] = output_dict.get("normalized_boxes")
+            item[f'{key_image_embedding}_features'] = output_dict.get("roi_features")
+    except:
+        item[f'{key_image_embedding}_boxes'] = None
+        item[f'{key_image_embedding}_features'] = None
+        log_file.append(item['wikipedia_title'])
     return item
 
 
@@ -162,9 +162,11 @@ if __name__ == '__main__':
     parser.add_argument('--key_image', type=str, required=True)
     parser.add_argument('--key_image_embedding', type=str, required=True)
     parser.add_argument('--image_path', type=str, required=False)
+    parser.add_argument('--log_path', type=str, required=False, default='')
     arg = parser.parse_args()
     kwargs = create_kwargs(arg)
     kwargs.pop('type')
+    log_path = kwargs.pop('log')
     if arg.type == 'CLIP':
         kwargs.pop("model_config_path")
         kwargs.pop("model")
@@ -174,3 +176,8 @@ if __name__ == '__main__':
         embed_with_fasterRCNN(**kwargs)
     else:
         raise NotImplementedError()
+    if log_path != '':
+        print(log_file)
+        with open(log_path, 'w'):
+            json.dump({"error":log_file})
+    
--- slurm_scripts/wikimage/embedding_fasterRCNN.sh
+++ slurm_scripts/wikimage/embedding_fasterRCNN.sh
@@ -4,20 +4,21 @@
 #SBATCH -J embedding_image
 #SBATCH --gres=gpu:1
 #SBATCH -p gpu
-#SBATCH --mem=24G
+#SBATCH --mem=40G
 #SBATCH --time=0-06:00:00
 
 source /home/pgrimal/.bashrc
 source activate cvlp
 
 python -m processing.embedding_image \
-    --dataset_path=/scratch_global/stage_pgrimal/data/CVLP/data/datasets/wikimage/wikimage_no_filter/validation \
+    --dataset_path=/scratch_global/stage_pgrimal/data/CVLP/data/datasets/wikimage/wikimage_no_filter \
     --type=FasterRCNN \
     --model_config_path=/scratch_global/stage_pgrimal/data/CVLP/data/frcnn_model/config.yaml \
     --model=/scratch_global/stage_pgrimal/data/CVLP/data/frcnn_model/pytorch_model.bin \
     --image_path=/scratch_global/stage_pgrimal/data/CVLP/data/datasets/wikimage/Commons_wikimage \
     --key_image=list_images \
     --key_image_embedding=fastrcnn \
-    --batch_size=4
+    --batch_size=4 \
+    --log_path=/home/pgrimal/CVLEP/error_embedding.json
 
 echo "done"
