c48b09c54b196c71916d4e81cb0a30e004eb6d74
Paul Grimal <grimal-paul@outlook.fr>
2022-07-12 18:25:00 +0200

    prepare proccessing  after embedding + add script to embed viquae

--- slurm_scripts/processing_pretrained_tasks/map_multimedia_wikimage_filter_split.sh
+++ slurm_scripts/processing_pretrained_tasks/map_multimedia_wikimage_filter_split.sh
@@ -10,23 +10,32 @@
 source /home/pgrimal/.bashrc
 source activate cvlp
 
-dataset1=
-dataset2=
-wikimage=
-title2index=
+dataset1=/scratch_global/stage_pgrimal/data/CVLP/data/datasets/wikimage/wikimage_split
+dataset2=/scratch_global/stage_pgrimal/data/CVLP/data/datasets/multimedia/filtered/multimedia_split
+wikimage=/scratch_global/stage_pgrimal/data/CVLP/data/datasets/wikimage/wikimage_no_filter/
+title2index=/scratch_global/stage_pgrimal/data/CVLP/data/datasets/wikimage/title2index.json
+key_b_FN=fastrcnn_boxes
+key_f_F=fastrcnn_features
+key_CLIP=clip_features
 
 python -m process_data.map_embedding \
     --dataset_path=${dataset1}
-    --key_features_FasterRCNN=
-    --key_boxes_FasterRCNN=
-    --key_featuresCLIP=
+    --key_features_FasterRCNN=${key_f_FN}
+    --key_boxes_FasterRCNN=${key_b_FN}
+    --key_featuresCLIP=${key_CLIP}
+    --wikimage_path=${wikimage}
+    --title2index_wikimage=${title2index}
+
+python -m process_data.map_embedding \
+    --dataset_path=${dataset2}
+    --key_features_FasterRCNN=${key_f_FN}
+    --key_boxes_FasterRCNN=${key_b_FN}
+    --key_featuresCLIP=${key_CLIP}
     --wikimage_path=${wikimage}
     --title2index_wikimage=${title2index}
 
 python -m process_data.remove_len2 \
+    --dataset_path=${dataset1}
+
+python -m process_data.remove_len2 \
     --dataset_path=${dataset2}
-    --key_features_FasterRCNN=
-    --key_boxes_FasterRCNN=
-    --key_featuresCLIP=
-    --wikimage_path=${wikimage}
-    --title2index_wikimage=${title2index}
---
+++ slurm_scripts/viquae/embedding_CLIP.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -J clip_viquae
+#SBATCH --gres=gpu:1
+#SBATCH -p gpu
+#SBATCH -w node7
+#SBATCH --mem=80G
+#SBATCH --time=1-00:00:00
+
+source /home/pgrimal/.bashrc
+source activate cvlp
+
+python -m processing.embedding_image \
+    --dataset_path=/scratch_global/stage_pgrimal/data/CVLP/data/datasets/miniviquae \
+    --type=CLIP \
+    --backbone=/scratch_global/stage_pgrimal/data/CVLP/data/clip/RN101.pt \
+    --image_path=/scratch_global/stage_pgrimal/data/miniViQuAE/data/dataset/miniCommons/ \
+    --key_image=image \
+    --key_image_embedding=clip \
+    --batch_size=4 \
+    --log_path=/home/pgrimal/CVLEP/error_embedding_clip_viquae.json
+
+echo "KB"
+python -m processing.embedding_image \
+    --dataset_path=/scratch_global/stage_pgrimal/data/CVLP/data/datasets/kb \
+    --type=CLIP \
+    --backbone=/scratch_global/stage_pgrimal/data/CVLP/data/clip/RN101.pt \
+    --image_path=/scratch_global/stage_pgrimal/data/miniViQuAE/data/dataset/miniCommons/ \
+    --key_image=image \
+    --key_image_embedding=clip \
+    --batch_size=4 \
+    --log_path=/home/pgrimal/CVLEP/error_embedding_clip_viquae.json
+
+echo "done"
