41b2aa92676433b80bb869451d219d16560de54f
Paul Grimal <grimal-paul@outlook.fr>
2022-07-25 15:27:39 +0200

    add config and slurm script

---
+++ slurm_scripts/training/bergamote/multitask/finetuning.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH --time=7-00:00:00
+#SBATCH --nodes=1
+#SBATCH -J freeze_proj
+#SBATCH --gres=gpu:2
+#SBATCH --ntasks-per-node=2
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=46G
+#SBATCH --nodelist=node7
+
+source /home/pgrimal/.bashrc
+source activate cvlp
+
+export MASTER_ADDR="$(scontrol show hostnames $SLURM_JOB_NODELIST | head -n1)"
+export MASTER_PORT=29700
+
+encoder_question_path=experiments/config_vladapter/factoryIA/embedding/encoder_question.json
+encoder_passage_path=experiments/config_vladapter/factoryIA/embedding/encoder_passage.json
+model_path=experiments/config_vladapter/factoryIA/embedding/config_model.json
+training_path=experiments/config_vladapter/factoryIA/finetuning/finetuning.json
+
+echo "projection et adapter freeze embedding"
+srun --kill-on-bad-exit=1 python -m cvlep.trainer_base_vladapter \
+    --encoder_question_path=${encoder_question_path} \
+    --encoder_passage_path=${encoder_passage_path} \
+    --model_path=${model_path} \
+    --training_path=${training_path}
+
+echo "The End"
+
