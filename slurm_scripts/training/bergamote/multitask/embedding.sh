ddd26df83c684b6cd8dea5d66257bde2d2558c82
Paul Grimal <grimal-paul@outlook.fr>
2022-07-28 11:44:21 +0200

    organization

---
+++ slurm_scripts/training/bergamote/multitask/embedding.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -J embed_passage
+#SBATCH --gres=gpu:1
+#SBATCH -p gpu
+#SBATCH -w node6
+#SBATCH --mem=40G
+#SBATCH --time=0-12:00:00
+
+source /home/pgrimal/.bashrc
+source activate cvlp
+
+# variables
+dataset=/scratch_global/stage_pgrimal/data/CVLP/data/datasets/zero_and_finetuning_test/vlt5_dataset
+kb=/scratch_global/stage_pgrimal/data/CVLP/data/datasets/zero_and_finetuning_test/kb
+passages=/scratch_global/stage_pgrimal/data/CVLP/data/datasets/zero_and_finetuning_test/passages
+
+config_passage_path=experiments/config_vladapter/bergamote/embedding/encoder_passage.json
+config_question_path=experiments/config_vladapter/bergamote/embedding/encoder_question.json
+config_model_path=experiments/config_vladapter/bergamote/embedding/config_model.json
+batch_size=128
+key_embedding=multitask_resnet_embedding
+
+
+echo "Passage"
+python -m processing.embedding_dataset \
+    --dataset_path=${passages} \
+    --type=passage \
+    --config_question_path=${config_question_path} \
+    --config_passage_path=${config_passage_path} \
+    --config_model_path=${config_model_path} \
+    --key_boxes=vlt5_normalized_boxes \
+    --key_vision_features=vlt5_features \
+    --key_text=passage \
+    --key_embedding=${key_embedding} \
+    --kb_path=${kb} \
+    --batch_size=${batch_size}
+
+echo "Done"
